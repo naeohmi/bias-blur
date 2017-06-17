f86d8be59a5432b8b47b5f90dcf1a81e28305c06
naeohmi <naeohmi@gmail.com>
2017-06-16 23:34:10 -0400

    bootstrap

--- aboutface-react/src/components/LandingPage.js
+++ aboutface-react/src/components/LandingPage.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
+import { Bootstrap, Carousel } from 'react-bootstrap';
 
 class LandingPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            index: 0,
+            direction: null
         }
+    }
+
+    handleSelect(selectedIndex, e) {
+        alert('selected=' + selectedIndex + ', direction=' + e.direction);
+        this.setState({
+            index: selectedIndex,
+            direction: e.direction
+        });
     }
 
     render() {
         return (
-            <div className="landing-wrapper">
-                <h1>Landing Page</h1>
-            </div>
+            <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect}>
+                <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src="/assets/carousel.png" />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src="/assets/carousel.png" />
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img width={900} height={500} alt="900x500" src="/assets/carousel.png" />
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         );
     }
 }
