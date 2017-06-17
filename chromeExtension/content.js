c2a916d5f32c197aeb1a09d94ab30908d6cad254
Vicky Chen <c.vicky1213@gmail.com>
2017-06-17 14:54:57 -0400

    updates

--- chromeExtension/content.js
+++ chromeExtension/content.js
@@ -1,42 +1,5 @@
-var elements = document.getElementsByTagNAme('*');
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
 
-// // for(var i=0; i<elements.length; i++){
-// // 	var element = elements[i];
-
-// // 	for (var j =0; j<element.childNodes.length; j++){
-// // 		var node = element.childNodes[j];
-
-// // 		if(node.nodeType ===3){
-// // 			var text = node.nodeValue;
-// // 			var replacedText = text.replace(/Linked/gi, 'give');
-
-// // 			if(replacedText !== text){
-// // 				element.replaceChild(document.createTextNode(replacedText), node)
-// // 			}
-// // 		}
-// // 	}
-// // }
-
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            replacedText = text.replace("Vicky, "VVV");
-
-            if (replacedText !== text) {
-                console.log('replaced');
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-    }
-
-
-chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tab){
-      chrome.tabs.executeScript(tab.id,{code:"document.title = 'My lame title!'"});
-   }
-);
